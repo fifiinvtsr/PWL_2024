ebb6d70b20ec8f0f0076c1529a87493c55f4850d
Fifi <nfifi4920@gmail.com>
2024-08-28 22:05:00 +0700

    Pertemua 1

---
+++ KTP_Fifi Novitasari/KTP_Fifi.php
@@ -0,0 +1,16 @@
+<?php
+$nik = "123456789101112134";
+$nama = "FIFI NOVITASARI";
+$tempat_lahir = "PASURUAN";
+$tanggal_lahir = "13-12-2003";
+$jenis_kelamin = "PEREMPUAN";
+$gol_darah = "-";
+$alamat = "JL. DR CIPTO Gg VIII";
+$rt_rw = "002/010";
+$kel_desa = "BEDALI";
+$kecamatan = "LAWANG";
+$agama = "ISLAM";
+$status_perkawinan = "BELUM KAWIN";
+$pekerjaan = "PELAJAR/MAHASISWA";
+$kewarganegaraan = "WNI";
+$berlaku_hingga = "SEUMUR HIDUP";
